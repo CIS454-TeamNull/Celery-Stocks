ff75b797750a2e5d8e5b997d80aeedea9cb14579
Manu <manu@localhost.localdomain>
2024-02-10 00:49:43 -0500

    testing templates and deployment

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
+import datetime
 from flask import (Flask, redirect, render_template, request, send_from_directory, url_for)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Celery Stocks by Team Null"
+    #return "Celery Stocks by Team Null"
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
    app.run()
