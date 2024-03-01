9d0247d20bf6adfd2d4c25c08c347485d3a435e7
Manu <>
2024-03-01 00:27:29 -0500

    Flask migrate, basic DB

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'database.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
