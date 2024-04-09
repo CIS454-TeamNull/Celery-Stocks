053e5bfc6604aab53d843237b6bf387864d151e1
csnemeth <91552959+csnemeth@users.noreply.github.com>
2024-04-09 16:52:27 -0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,65 @@
+from datetime import datetime, timezone
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from hashlib import md5
+
+item = {}
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+
+class User(UserMixin, db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    account_type: so.Mapped[Optional[str]] = so.mapped_column(
+        sa.String(64), index=True, default="viewer"
+    )
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
+
+    def __repr__(self):
+        return "<User {}>".format(self.username)
+
+
+class Item(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(140), index=True, unique=True)
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc)
+    )
+    supply: so.Mapped[int] = so.mapped_column(index=True)
+
+    def __repr__(self):
+        return "<Item {}>".format(self.name)
+
+class Waiter(User):
+    account_type: so.Mapped[Optional[str]] = so.mapped_column(
+        sa.String(64), index=True, default="waiter"
+    )
+        
+class Chef(User):
+    so.Mapped[Optional[str]] = so.mapped_column(
+        sa.String(64), index=True, default="chef"
+    )
+
+class MenuItem(Item):
+    ingredints = []
