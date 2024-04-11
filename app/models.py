4cb7208dc4590849fab12119d4d875e8c2c9277b
Manu <>
2024-04-11 11:08:10 -0400

    Basic profanity filter implemented using alt-profanity-check

--- app/models.py
+++ app/models.py
@@ -13,6 +13,8 @@ def load_user(id):
     return db.session.get(User, int(id))
 
 
+menu_items = sa.Table('menu_items', db.metadata, sa.Column('item_id', sa.Integer, sa.ForeignKey('item.id'), primary_key=True), sa.Column('menu_id', sa.Integer, sa.ForeignKey('menu.id'), primary_key=True))
+
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
@@ -24,7 +26,7 @@ class User(UserMixin, db.Model):
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc)
     )
-
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -34,10 +36,20 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
         return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
-
     def __repr__(self):
         return "<User {}>".format(self.username)
 
+class Menu(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(140), index=True, unique=True)
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc)
+    )
+    #items = so.WriteOnlyMapped['Item'] = so.relationship(back_populates='menu')
+    items = db.relationship('Item', secondary=menu_items, backref='item')
+
+    def __repr__(self):
+        return "<Menu {}>".format(self.name)
 
 class Item(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -47,8 +59,13 @@ class Item(db.Model):
     )
     supply: so.Mapped[int] = so.mapped_column(index=True)
 
+    menu_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Menu.id), index=True)
+
+    menu: so.Mapped[Menu] = so.relationship(back_populates='items')
+
     def __repr__(self):
         return "<Item {}>".format(self.name)
+
 
 class Waiter(User):
     # account_type: so.Mapped[Optional[str]] = so.mapped_column(
@@ -66,8 +83,3 @@ class Chef(User):
         for i in ingredients:
             db.session.add(ingredients[i])
         db.session.commit()
-    
-    #    item.update({menuItem : ingredients})
-        #   account_type: so.Mapped[Optional[str]] = so.mapped_column(
-    #    sa.String(64), index=True, default="waiter"
-    # )
